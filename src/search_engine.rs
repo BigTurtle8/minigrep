9a4f94a9662a59bec9e6ad50e9459f902b60fc4c
Marcus Alagar <mvala079@gmail.com>
2025-07-22 14:04:49 -0500

    Separated search functionality into own file
    Using chapter 7, separate the search functionality into its
    own module and file, `search_engine.rs`. All that needed to
    do was declare `mod search_engine;` in `lib.rs` and then
    create a `search_engine.rs` to paste the code into.
    
    Also added docstrings for `search_engine`.

---
+++ src/search_engine.rs
@@ -0,0 +1,63 @@
+//! Contains code for searching through strings
+//! by line.
+
+/// Search each line of `contents` for those that
+/// contain `query`, and return a vector containing
+/// all of those lines.
+///
+/// Is case-sensitive.
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
+}
+
+/// Search each line of `contents` for those that
+/// contain `query`, and return a vector containing
+/// all of those lines.
+///
+/// Is case-insensitive.
+pub fn search_case_insensitive<'a>(
+    query: &str,
+    contents: &'a str
+) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query))
+        .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case_sensitive() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
+    }
+}
